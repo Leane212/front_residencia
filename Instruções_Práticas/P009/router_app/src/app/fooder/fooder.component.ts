9d9a627c6ddd029b910a5bbe089199e8e9d031f2
leane <leanesoares212@gmail.com>
2024-02-04 22:08:22 -0300

    Atividade P009

---
+++ Instruções_Práticas/P009/router_app/src/app/fooder/fooder.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fooder',
+  templateUrl: './fooder.component.html',
+  styleUrl: './fooder.component.css'
+})
+export class FooderComponent {
+
+}
---
+++ Instruções_Práticas/P009/router_app/src/app/siteuesc/siteuesc.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-siteuesc',
+  templateUrl: './siteuesc.component.html',
+  styleUrl: './siteuesc.component.css'
+})
+export class SiteuescComponent {
+}
